f3437c98a6b96d8d29c54469f52a92814446d235
Santiago Carletti <111025122+santiagocarletti@users.noreply.github.com>
2022-08-15 18:55:52 -0300

    Update Program.cs

--- Unidad 1/Ejercicio 1/Program.cs
+++ Unidad 1/Ejercicio 1/Program.cs
@@ -1,10 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// Ejercicio 1
+// ENUNCIADO EJERCICIO 1
 // Esta unidad es más teórica introductoria que para ejercitar. Por eso el siguiente ejercicio 
 // va a ser más que nada para que testeemos que el entorno quedó configurado correctamente y 
 // que estamos en condiciones para comenzar a programar.
-// 1. Para este primer ejercicio deberás crear tu primer programa que emita en pantalla "Hola Mundo".
+//
+// Para este primer ejercicio deberás crear tu primer programa que emita en pantalla "Hola Mundo".
 // Adicionalmente, podés hacer las modificaciones correspondientes para que el programa emita 
 // "Bienvenido Juanito", donde "Juanito" puede ser tu nombre.
 
