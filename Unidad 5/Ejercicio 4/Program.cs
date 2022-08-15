3cb4581fcce4ca49a4f1f5e1ea646ca775816dcb
Santiago Carletti <111025122+santiagocarletti@users.noreply.github.com>
2022-08-15 20:31:55 -0300

    Update Program.cs

--- Unidad 5/Ejercicio 4/Program.cs
+++ Unidad 5/Ejercicio 4/Program.cs
@@ -4,7 +4,7 @@ class Program
     static void Main(string[] args)
     {
 
-        //enunciado ejercicio 4
+        //ENUNCIADO EJERCICIO 4
         //Hacer un programa que solicite UN número y luego calcule y emita un cartel
         //aclaratorio si el mismo es primo o no es primo.  Nota: un 
         //numero es primo cuando es divisible únicamente por 1 y por sí mismo.
